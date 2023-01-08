7c4452b47a21029d82d1b7718673fa4751c58f60
Devisione <stipafk@gmail.com>
2023-01-08 12:43:58 +0300

    fix usage

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { connectContext } from "./connectContext";
 import { withContexts } from "./withContexts";
-import { createStore } from "./store/createStore";
+import { createStore } from "./createStore";
 
 export { withContexts, connectContext, createStore };
